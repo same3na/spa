9d8ae2666801094a7e1ef37a9937850efa98c1ab
Mona Lisa <namirabboud@gmail.com>
2025-06-13 10:06:12 +0200

    update

--- src/pages/classifications/ClassificationCreate.tsx
+++ src/pages/classifications/ClassificationCreate.tsx
@@ -281,13 +281,13 @@ export default function ClassificationCreate() {
                   { key: "album_name", label: "Album Name" },
                   { key: "custom", label: "Satus" , render: (row: Song) => (
                     <span 
-                      className={"inline-flex items-center rounded-md text-xs font-medium ring-1 ring-inset p-1 " + (row.analyze_success.Bool  === true ? "bg-green-50 text-green-700 ring-green-600/20" : row.analyze_success.Valid === null ? "bg-blue-50 text-blue-700 ring-blue-700/10" : "bg-red-50 text-red-700 ring-red-600/10")}>
-                        {row.analyze_success.Bool  === true ? "Success" : row.analyze_success.Valid  === false ? "Pending" : "Failed"}
+                      className={"inline-flex items-center rounded-md text-xs font-medium ring-1 ring-inset p-1 " + (row.analyze_success.bool  === true ? "bg-green-50 text-green-700 ring-green-600/20" : row.analyze_success.valid === null ? "bg-blue-50 text-blue-700 ring-blue-700/10" : "bg-red-50 text-red-700 ring-red-600/10")}>
+                        {row.analyze_success.bool  === true ? "Success" : row.analyze_success.valid  === false ? "Pending" : "Failed"}
                     </span>
                   )},
                   { key: "custom", label: "Action", render: (row: Song) => (
                     <button 
-                      disabled={row.analyze_success.Bool  === true ? false : true}
+                      disabled={row.analyze_success.bool  === true ? false : true}
                       className="rounded bg-slate-800 py-1 px-2.5 border border-transparent text-center text-sm text-white transition-all shadow-sm hover:shadow focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" 
                       type="button"
                       onClick={() => {
